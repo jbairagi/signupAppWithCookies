81b2f4238da6792da5fefab838e1b28ee42851cd
Jayesh <jbairagi@grepruby.com>
2017-09-13 20:01:59 +0530

    checking previously registered users with new entry during signup

--- controller/signup.js
+++ controller/signup.js
@@ -8,22 +8,29 @@ exports.signupCreate =  function(req, res){
   var username = req.body.username;
   var pass = req.body.password;
   var email = req.body.email;
-   if(!username || !pass || !email){
-      res.send("Invalid details!");
-   }
-   else{
-     var newUser = new User({
-       username: username,
-       emailId: email,
-       password: pass
-     });
-     newUser.save(function(err, user){
-       if(err)
-            res.render('signup', {message: "Database error", type: "error"});
-         else{
-           res.cookie('loginId', user._id, {httpOnly: true, signed: true, maxAge: 360000});
-           res.redirect('/profile');
-          }
-     });
-   }
+
+  User.count({ $or:[ {'emailId': email}, {'username': username}]}, function (err, count) {
+      if (count === 0) {
+        if(!username || !pass || !email){
+           res.send("Invalid details!");
+        }
+        else{
+          var newUser = new User({
+            username: username,
+            emailId: email,
+            password: pass
+          });
+          newUser.save(function(err, user){
+            if(err)
+                 res.render('signup', {message: "Database error", type: "error"});
+              else{
+                res.cookie('loginId', user._id, {httpOnly: true, signed: true, maxAge: 360000});
+                res.redirect('/profile');
+               }
+          });
+        }
+      }
+      else{res.render('signup', {message: "Username or email already registered! Signup Again!"});
+      }
+  });
 };
