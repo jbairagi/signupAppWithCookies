5d9c6aabd6a7e167bc911dd7c9c3d95171c9c3cd
Jayesh <jbairagi@grepruby.com>
2017-09-13 20:03:17 +0530

    session router modified

--- routes/session.js
+++ routes/session.js
@@ -4,7 +4,7 @@ var session = require('./../controller/session');
 var check = require('./../middlewares/checks.js');
 
 router.get('/login', check.userLoggedIn, session.sessionNew);
-router.post('/login', check.authenticate, session.sessionCreate);
+router.post('/login', session.sessionCreate);
 
 router.get('/logout', session.sessionDestroy);
 
