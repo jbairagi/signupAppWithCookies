753d982404787e7beb82b956503c0f74f2eb289e
Jayesh <jbairagi@grepruby.com>
2017-09-14 13:06:38 +0530

    middleware added to check logged in user

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var profile = require('./../controller/profile');
+var check = require('./../middlewares/checks.js');
 
-router.get('/profile', profile.profile);
+router.get('/profile', check.assignUser, profile.profile);
 
 module.exports = router;
