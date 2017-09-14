a0fbc1bea784a11eed64780b5597cc95df49e1fe
Jayesh <jbairagi@grepruby.com>
2017-09-14 13:04:03 +0530

    checks.js modified to check logged in user

--- middlewares/checks.js
+++ middlewares/checks.js
@@ -6,10 +6,28 @@ exports.userLoggedIn = function(req, res, next){
     User.findById(userId, (err, user) => {
         if (err)
           console.log(err);
+        req.userData = user;
+        //console.log(user);
         res.redirect('/profile');
       });
   }
   else{
     next();
+  }
+}
+
+
+exports.assignUser = function(req, res, next){
+  let userId = req.signedCookies['loginId'];
+  if(userId){
+    User.findById(userId, (err, user) => {
+        if (err)
+          console.log(err);
+        req.userData = user;
+        next();
+      });
+  }
+  else{
+    res.redirect('/signup');
   }
 }
