f14cdef3dade19acbef853a2d13b784d8458b783
Jayesh <jbairagi@grepruby.com>
2017-09-17 23:49:46 +0530

    functions to edit projects added

--- controller/manageProjectController.js
+++ controller/manageProjectController.js
@@ -6,7 +6,7 @@ exports.manageProjectNew = function(req, res){
     res.redirect('/profile');
 };
 
-exports.projectCreate =  function(req, res){
+exports.projectCreate = function(req, res){
   var title = req.body.title;
   var description = req.body.description;
   var beginningDate = req.body.beginningDate;
@@ -37,11 +37,6 @@ exports.projectCreate =  function(req, res){
                 res.redirect('/login');
               }
               else {
-                //console.log(project);
-                // User.findById(dev, function(err, response){
-                //   console.log(response);
-                // });
-
                 User.findByIdAndUpdate(dev, {$push: {'project': project._id}},
                   function(err, response){
                       //console.log(response);
@@ -52,20 +47,9 @@ exports.projectCreate =  function(req, res){
             });
           }
           else{
-
-            console.log(dev);
-            Project.find({'title': title}, function(err, project){
-              User.findByIdAndUpdate(dev, {$push: {'project': project._id}},
-                function(err, response){
-                    //console.log(response);
-              });
-            });
+            console.log("Project already exists. Update project details to add a developer");
             res.redirect('/profile');
-            console.log("down");
-
-
-
-
+            //console.log("down");
           }
         });
       }
@@ -77,11 +61,120 @@ exports.projectCreate =  function(req, res){
   });
 };
 
-exports.getAssignedProjects = function(req, res){
+exports.addProjectDeveloper = function(req, res){
   var user = req.userData;
-
+  var title = req.body.title;
+  var developer = req.body.developer;
+  User.find({'username': developer}, function(err, user){
+    if(err)
+      console.log(err);
+    if(user[0]){
+      dev = user[0]._id;
+      if(!title || !dev){
+        res.send("Invalid details!");
+      }
+      else{
+        Project.count({'title': title}, function(err, count){
+          if(count === 0){
+            console.log("Project " + title + " is not added. First add the project.");
+            res.redirect('/profile');
+          }
+          else{
+            //console.log(dev);
+            Project.findOneAndUpdate({'title': title}, {$push: {'developers': dev}}, function(err, project){
+              console.log(project);
+              User.findByIdAndUpdate(dev, {$push: {'project': project._id}},function(err, user){
+                console.log(user);
+              });
+            });
+            res.redirect('/profile');
+            //console.log("down");
+          }
+        });
+      }
+    }
+    else{
+      console.log(developer +  " is not registered");
+      res.redirect('/profile');
+    }
+  });
 };
 
+exports.editProjectDescription = function(req, res){
+  var user = req.userData;
+  var title = req.body.title;
+  var description = req.body.description;
+  Project.count({'title': title}, function(err, count){
+    if(count === 0){
+      console.log("Project " + title + " is not added. First add the project.");
+      res.redirect('/profile');
+    }
+    else{
+      //console.log(dev);
+      Project.findOneAndUpdate({'title': title}, {'description': description}, function(err, project) {
+        console.log(project);
+      });
+      res.redirect('/profile');
+      //console.log("down");
+    }
+  });
+};
+
+exports.editProjectBeginningDate = function(req, res){
+  var user = req.userData;
+  var title = req.body.title;
+  var beginningDate = req.body.beginningDate;
+  Project.count({'title': title}, function(err, count){
+    if(count === 0){
+      console.log("Project " + title + " is not added. First add the project.");
+      res.redirect('/profile');
+    }
+    else{
+      //console.log(dev);
+      Project.findOneAndUpdate({'title': title}, {'beginningDate': beginningDate}, function(err, project) {
+        console.log(project);
+      });
+      res.redirect('/profile');
+      //console.log("down");
+    }
+  });
+};
+
+exports.editProjectDueDate = function(req, res){
+  var user = req.userData;
+  var title = req.body.title;
+  var dueDate = req.body.dueDate;
+  Project.count({'title': title}, function(err, count){
+    if(count === 0){
+      console.log("Project " + title + " is not added. First add the project.");
+      res.redirect('/profile');
+    }
+    else{
+      //console.log(dev);
+      Project.findOneAndUpdate({'title': title}, {'dueDate': dueDate}, function(err, project) {
+        console.log(project);
+      });
+      res.redirect('/profile');
+      //console.log("down");
+    }
+  });
+};
+
+exports.removeProject = function(req, res){
+  var user = req.userData;
+  var title = req.body.title;
+  Project.count({'title': title}, function(err, count){
+    if(count === 0){
+      console.log("Project " + title + " is not added.");
+      res.redirect('/profile');
+    }
+    else{
+      Project.findOneAndRemove({'title': title});
+      res.redirect('/profile');
+      //console.log("down");
+    }
+  });
+};
 
 
 
--- controller/profileController.js
+++ controller/profileController.js
@@ -2,5 +2,9 @@ var User = require('./../models/user');
 
 exports.profile = function(req, res){
   var user = req.userData;
-  res.render('profile', {username: user.username, type: user.role});
+  // for(var i=0; i<user){
+  //
+  // }
+  console.log(user.project);
+  res.render('profile', {username: user.username, type: user.role, projects: user.project});
 };
