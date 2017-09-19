994d3b67ab8aa23157cf93508853517955c7d9f2
Jayesh <jbairagi@grepruby.com>
2017-09-19 18:02:36 +0530

    changed middleware on checking

--- routes/addUserRoutes.js
+++ routes/addUserRoutes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var addUser = require('./../app/controller/addUserController');
 var check = require('./../app/middlewares/checks.js');
 
-router.get('/addUser',  check.assignUser, addUser.addUserNew);//check
+router.get('/addUser', check.isManager, addUser.addUserNew);
 router.post('/addUser', check.isManager, addUser.addUserCreate);
 
 module.exports = router;
