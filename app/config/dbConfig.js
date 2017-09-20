eacfdd2b8dc102ff246a8f6705aab1638f752dee
Jayesh <jbairagi@grepruby.com>
2017-09-20 18:58:48 +0530

    using local db

--- app/config/dbConfig.js
+++ app/config/dbConfig.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-// var db = mongoose.createConnection('mongodb://localhost/mydb');
-var db = mongoose.createConnection('mongodb://jayeshbairagi:jaymlab1@ds141264.mlab.com:41264/mydb');
+var db = mongoose.createConnection('mongodb://localhost/mydb');
+// var db = mongoose.createConnection('mongodb://jayeshbairagi:jaymlab1@ds141264.mlab.com:41264/mydb');
 db.once('open', (err) => {
   if (!err)
   console.log("connected successfully!")
